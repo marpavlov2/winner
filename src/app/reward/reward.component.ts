e8006fc0be62edae41a85cb6267338517f1f9c3d
Mario Pavlovic <mariopavlovic1994@gmail.com>
2024-04-05 17:30:07 +0200

    style changes

--- src/app/reward/reward.component.ts
+++ src/app/reward/reward.component.ts
@@ -1,13 +1,18 @@
 import { Component, Input } from '@angular/core';
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 
 @Component({
   selector: 'app-reward',
   standalone: true,
-  imports: [],
+  imports: [LottieComponent],
   templateUrl: './reward.component.html',
   styleUrl: './reward.component.scss',
 })
 export class RewardComponent {
+  options: AnimationOptions = {
+    path: '/assets/earnplaying.json',
+  };
+
   @Input()
   lockedBalance: number;
 }
