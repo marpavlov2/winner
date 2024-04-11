133ca765d6a4c8bad54bc1a782ddb3ca0fd75578
Mario Pavlovic <mariopavlovic1994@gmail.com>
2024-04-11 02:46:51 +0200

    add number

--- src/app/reward/reward.component.ts
+++ src/app/reward/reward.component.ts
@@ -8,11 +8,19 @@ import {
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
 import { SetBetComponent } from '../set-bet/set-bet.component';
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
+import { AnimationItem } from 'lottie-web';
 
 @Component({
   selector: 'app-reward',
   standalone: true,
-  imports: [NgIconComponent, SetBetComponent, MatSnackBarModule, FormsModule],
+  imports: [
+    NgIconComponent,
+    SetBetComponent,
+    MatSnackBarModule,
+    FormsModule,
+    LottieComponent,
+  ],
   providers: [
     provideIcons({
       heroRocketLaunch,
@@ -25,8 +33,22 @@ import { SetBetComponent } from '../set-bet/set-bet.component';
   styleUrl: './reward.component.scss',
 })
 export class RewardComponent {
+  options: AnimationOptions = {
+    path: '../../../assets/logo-animation-1-w.json',
+  };
+
   @Input()
   lockedBalance: number;
 
+  @Input()
+  luckyTicket: number;
+
+  private animationItem: AnimationItem;
+
+  animationCreated(animationItem: AnimationItem): void {
+    this.animationItem = animationItem;
+
+    this.animationItem.playSpeed = 0.8;
+  }
   constructor() {}
 }
