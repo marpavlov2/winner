37202fa221fdd919215c270f813721624b6e3ed1
Mario Pavlovic <mariopavlovic1994@gmail.com>
2024-05-01 17:59:23 +0200

    navigation fix

--- src/app/header/navigation.component.ts
+++ src/app/header/navigation.component.ts
@@ -6,7 +6,6 @@ import {
   provideNgIconsConfig,
 } from '@ng-icons/core';
 import { MatDialog } from '@angular/material/dialog';
-import { GameInfoDialogComponent } from '../game-info-dialog/game-info-dialog.component';
 import { MainContractService } from '../services/main-contract.service';
 import { LottieComponent, AnimationOptions } from 'ngx-lottie';
 import { TonConnectService } from '../ton-connect.service';
@@ -15,12 +14,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [
-    NgIconComponent,
-    RouterModule,
-    GameInfoDialogComponent,
-    LottieComponent,
-  ],
+  imports: [NgIconComponent, RouterModule, LottieComponent],
   providers: [
     provideIcons({
       heroTrophy,
@@ -51,13 +45,5 @@ export class NavigationComponent {
 
   openMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
-  }
-
-  openInfoDialog(): void {
-    const dialogRef = this.dialog.open(GameInfoDialogComponent, {
-      width: '880px',
-    });
-
-    dialogRef.componentInstance.fee = this.mainContractService.feePercent;
   }
 }
